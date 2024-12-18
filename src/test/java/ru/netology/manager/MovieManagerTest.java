3c878c321abcbe9011d5a8898edd060480ce9394
Darya Jenkins <melissegreenleaf@gmail.com>
2024-12-18 23:26:50 +0300

    Homework edited

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -6,14 +6,21 @@ import org.junit.jupiter.api.Test;
 public class MovieManagerTest {
 
     @Test
-    public void noMovies() {
+    public void findAllIfNoMovies() {
         MovieManager manager = new MovieManager();
         String[] expected = {};
         Assertions.assertArrayEquals(expected, manager.findAll());
     }
 
     @Test
-    public void onlyOneMovie() {
+    public void findLastIfNoMovies() {
+        MovieManager manager = new MovieManager();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, manager.findLast());
+    }
+
+    @Test
+    public void findAllIfOnlyOneMovie() {
         MovieManager manager = new MovieManager();
         manager.addMovie("Вперёд");
         String[] expected = { "Вперёд" };
@@ -21,7 +28,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void allMovies() {
+    public void findAllTheSevenMovies() {
         MovieManager manager = new MovieManager();
         manager.addMovie("Бладшот");
         manager.addMovie("Вперёд");
@@ -36,7 +43,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void showFiveReversedMovies() {
+    public void findLastIfMoviesMatchLimit() {
         MovieManager manager = new MovieManager();
         manager.addMovie("Бладшот");
         manager.addMovie("Вперёд");
@@ -49,7 +56,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void showReversedMoviesUnderLimit() {
+    public void findLastIfMoviesUnderLimit() {
         MovieManager manager = new MovieManager();
         manager.addMovie("Бладшот");
         manager.addMovie("Вперёд");
@@ -61,7 +68,31 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void showThreeReversedMovies() {
+    public void findLastIfMoviesAboveLimit() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Вперёд");
+        manager.addMovie("Отель «Белград»");
+        manager.addMovie("Джентльмены");
+        manager.addMovie("Человек-невидимка");
+        manager.addMovie("Тролли. Мировой тур");
+        manager.addMovie("Номер один");
+
+        String[] expected = { "Номер один", "Тролли. Мировой тур", "Человек-невидимка", "Джентльмены", "Отель «Белград»" };
+        Assertions.assertArrayEquals(expected, manager.findLast());
+    }
+
+    @Test
+    public void findOnlyOneLastMovie() {
+        MovieManager manager = new MovieManager(1);
+        manager.addMovie("Вперёд");
+        manager.addMovie("Отель «Белград»");
+        manager.addMovie("Джентльмены");
+        String[] expected = {"Джентльмены"};
+        Assertions.assertArrayEquals(expected, manager.findLast());
+    }
+
+    @Test
+    public void findThreeLastMovies() {
         MovieManager manager = new MovieManager(3);
         manager.addMovie("Человек-невидимка");
         manager.addMovie("Тролли. Мировой тур");
@@ -72,7 +103,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void showSevenReversedMovies() {
+    public void findSevenLastMovies() {
         MovieManager manager = new MovieManager(7);
         manager.addMovie("Бладшот");
         manager.addMovie("Вперёд");
